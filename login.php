cf1344e3c0a6078f5fd896ae87488cb690530b57
Philip Marfo <philcho98@gmail.com>
2021-04-02 01:20:01 +0000

    Login HTML structure

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="container">
+    <label for="Email">Email</label>
+    <input type="text" name="email" id="email" required>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" required>
+    </div>
+
+ 
+
+</body>
+</html>
